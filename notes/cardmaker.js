45c2fd9a3eac1b748a9a1d7e3e140668d83b4990
inf-null-0 <119103652+Infi-null-0@users.noreply.github.com>
2024-06-15 08:13:04 +0530

    added offset in card creation

--- notes/cardmaker.js
+++ notes/cardmaker.js
@@ -1,9 +1,20 @@
 let buttons = document.querySelectorAll(".btn");
 
+// this makes sure the card doesn't overlap while creation.
+let createOffset = 0;
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     // console.log(button.dataset.color);
-    let card_e = new NotesCard(null, "", button.dataset.color, 20, 80);
+    let card_e = new NotesCard(
+      null,
+      "",
+      button.dataset.color,
+      20 + createOffset,
+      80 + createOffset
+    );
+    // increase the offset
+    createOffset += 16;
     card_e.addCard();
     makePopup("success", "Note added");
     // console.log(card_e.top, card_e.left);
